80d9898cdfd112f6716b10d1483f55d642d35497
Gurneet Arora <gurneet.k.arora@gmail.com>
2015-03-09 14:17:59 -0400

    nonparam est

--- main.m
+++ main.m
@@ -1,7 +1,9 @@
 load('./BIG DATER/lab2_1.mat');
-parametric_gaussian_ml(a,b);
-parametric_exponential(a,b);
-parametric_uniform(a,b); 
-nonparametric_parzen_window(a,b);
+% parametric_gaussian_ml(a,b);
+% parametric_exponential(a,b);
+% parametric_uniform(a,b); 
+% nonparametric_parzen_window(a,b);
 
-load('./BIG DATER/lab2_3.mat');
+load('./BIG DATER/lab2_2.mat');
+
+%load('./BIG DATER/lab2_3.mat');
---
+++ nonparam_est.m
@@ -0,0 +1,8 @@
+function [ output_args ] = nonparam_est( input_args )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+end
+
