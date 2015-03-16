46cfbb1c6f6e122f93fb049630d3cd4c71eae30e
Vishal Mathur <vishmathur5@gmail.com>
2015-03-16 19:24:12 -0400

    fixed a lot of bugs with the previous discriminant code

--- sequential_discriminants.m
+++ sequential_discriminants.m
@@ -2,21 +2,25 @@ function [ output_args ] = sequential_discriminants(a,b)
     a_pool = a;
     b_pool = b;
     
+    discriminants = [];
+    
     should_continue = true;
     
-    for j = 1:3
+    for k = 1:3
+        k
         while should_continue
-
         %   select 2 random points from a and b
-            r = randi([1 length(a_pool)],1,1);
+            length(a_pool)
+            length(b_pool)
+            
+            rA = randi([1 length(a_pool)],1,1);
+            rB = randi([1 length(b_pool)],1,1);
+            a_point = a_pool(rA,:);
+            b_point = b_pool(rB,:);            
 
-            a_point = a_pool(r);
-            a_pool(r) = [];
-            b_point = b_pool(r);
-            b_pool(r) = [];
-
+            % make grid
             grid_step = 1;
-            [xValuesAB, yValuesAB, MED_AB] = makeGrid2(grid_step, a, b);
+            [xValuesAB, yValuesAB, MED_AB] = makeGrid2(grid_step, a_pool, b_pool);
 
             AB_means = [a_point;b_point];
 
@@ -28,11 +32,25 @@ function [ output_args ] = sequential_discriminants(a,b)
                     MED_AB(i,j)=class;
                 end
             end
+            
+            % draw points and MED
+            
+            figure(999)
+            scatter(a_pool(:,1), a_pool(:,2), 20, 'r', 'filled')
+            hold on
+            scatter(b_pool(:,1), b_pool(:,2), 20, 'g', 'filled')
+            
+            scatter(a_point(1), a_point(2), 20, 'b', 'filled')
+            scatter(b_point(1), b_point(2), 20, 'm', 'filled')
 
+            q = contour(xValuesAB, yValuesAB, MED_AB, 1, 'r');
+            legend('A','B','Decision Boundary');
+            hold off
+            
+            % confusion matrix
             med_AB_conf = zeros(2,2);
-            %MED (1, 2, 3 metric)
-            for i = 1:size(a,1)
-                z = [a(i,1), a(i,2)];
+            for i = 1:size(a_pool,1)
+                z = [a_pool(i,1), a_pool(i,2)];
                 class = med(AB_means, z);
                 if(class==1)
                     med_AB_conf(1,1) = med_AB_conf(1,1)+1;
@@ -40,30 +58,64 @@ function [ output_args ] = sequential_discriminants(a,b)
                     med_AB_conf(1,2) = med_AB_conf(1,2)+1;
                 end
             end
-            for i = 1:size(b,1)
-                z = [b(i,1), b(i,2)];
+            for i = 1:size(b_pool,1)
+                z = [b_pool(i,1), b_pool(i,2)];
                 class = med(AB_means, z);
-                if(class==1)
-                    med_AB_conf(2,1) = med_AB_conf(2,1)+1;
+                if(class==2)
+                   med_AB_conf(2,2) = med_AB_conf(2,2)+1;
                 else
-                    med_AB_conf(2,2) = med_AB_conf(2,2)+1;
+                   med_AB_conf(2,1) = med_AB_conf(2,1)+1;
                 end
             end
-
-            figure(2)
-            scatter(a(:,1), a(:,2), 20, 'r', 'filled')
-            hold on
-            scatter(b(:,1), b(:,2), 20, 'g', 'filled')
-            contour(xValuesAB, yValuesAB, MED_AB, 1, 'r');
-            legend('A','B','Decision Boundary');
-            hold off
-
             med_AB_conf
-            if(isempty(a_pool)==1 || (med_AB_conf(1,1)==0 && med_AB_conf(2,2)==0))
-               should_continue = false
+            
+            if(med_AB_conf(1,2)==0 || med_AB_conf(2,1)==0 || isempty(a_pool)==1)
+                should_continue = false;
+                
+                discriminants{k} = q;
+                
+%                 figure(k)
+%                 scatter(a_pool(:,1), a_pool(:,2), 20, 'r', 'filled')
+%                 hold on
+%                 scatter(b_pool(:,1), b_pool(:,2), 20, 'g', 'filled')
+%                 contour(xValuesAB, yValuesAB, MED_AB, 1, 'r');
+%                 legend('A','B','Decision Boundary');
+%                 hold off
+                
+                %remove successfully classified points
+            
+                b_temp=[];
+                if(med_AB_conf(1,2)==0)
+                  % remove class B classified as B
+                    for i = 1:size(b_pool,1)
+                        z = [b_pool(i,1), b_pool(i,2)];
+                        class = med(AB_means, z);
+                        if(class==1)
+                          b_temp  = [b_temp; z];
+                        end
+                    end
+                    b_pool = b_temp;
+                end
+
+                a_temp = [];
+                if(med_AB_conf(2,1)==0)
+                  % remove class A classified as A
+                    for i = 1:size(a_pool,1)
+                        z = [a_pool(i,1), a_pool(i,2)];
+                        class = med(AB_means, z);
+                        if(class==0)
+                           a_temp = [a_temp; z];
+                        end
+                    end
+                    a_pool = a_temp;
+
+                end   
+
             end
         end
+        should_continue = true;
     end
+    discriminants
 end
 
 
